61708d71be0b1908639fbea7212b6b99247544d2
jomorro <41160359+jomorro@users.noreply.github.com>
2018-08-30 15:17:11 -0400

    orignal files

---
+++ index.js
@@ -0,0 +1,32 @@
+function alphabetize(a) {
+    return a.toLowerCase().split("").sort().join("").trim();
+}
+
+let anagramSets = {};
+
+for (let i = 0; i < words.length; i++) {
+    anagramSets[alphabetize(words[i])] = [];
+}
+
+for (i = 0; i < words.length; i++) {
+    let alphWord = alphabetize(words[i]);
+    if (alphWord in anagramSets) {
+        anagramSets[alphWord].push(words[i]);
+    }
+}
+
+function newList(text) {
+    const newList = document.createElement("li");
+    const newText = document.createTextNode(text);
+    newList.appendChild(newText);
+    document.getElementById("list").appendChild(newList);
+}
+
+let listFull = [];
+
+for (let i = 0; i < words.length; i++) {
+    if (anagramSets[alphabetize(words[i])].length >= 5 && alphabetize(words[i]) in listFull === false) {
+        newList(alphabetize(words[i]) + ": [" + anagramSets[alphabetize(words[i])] + "]");
+        listFull[alphabetize(words[i])] = [];
+    }
+}
